8aa3d382cffebaaa9d586af0e429af006ae3dabf
AlaianChicamoh <alainchiamoh@gmail,com>
2023-06-20 13:36:28 -0700

    Setup Creating user

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import 'firebase/auth';
+import { getAuth } from "firebase/auth";
+//import {getFirestore} from 'firebase/'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBJd9xLLY3Q2xhowgrUGm-SpnDGoY02po4",
+  authDomain: "newproject-ed6c5.firebaseapp.com",
+  projectId: "newproject-ed6c5",
+  storageBucket: "newproject-ed6c5.appspot.com",
+  messagingSenderId: "653991066389",
+  appId: "1:653991066389:web:5066a3a904fe16e92ac71c"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore();
